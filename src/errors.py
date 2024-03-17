79e7c534a54be84d56709cd6c4ddf13902593941
Ben Fasoli <benfasoli@gmail.com>
2024-03-17 19:55:35 +0100

    Add unhandled exception docstring

--- src/errors.py
+++ src/errors.py
@@ -6,4 +6,13 @@ logger = logging.getLogger(__name__)
 
 
 async def log_unhandled_exception(exc: Exception, _: Scope) -> None:
+    """Pass unhandled exceptions to log formatter.
+
+    Uncaught exception tracebacks are not logged by Litestar by default. The logging
+    interface is likely to change in Litestar v3.0, see:
+        https://github.com/litestar-org/litestar/issues/2858
+
+    This ensures unhandled exceptions are logged using the same formatter as other
+    non-error log messages.
+    """
     logger.error("Unhandled exception", exc_info=exc)
